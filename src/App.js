d49bba1ee8a431cf75ad8662d40e0da3b22d027a
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-11-30 09:31:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Rick & Morty <span className="text-primary"> Wiki </span></h1>
+      <h1 className="text-center ubuntu my-4">
+        Rick & Morty <span className="text-primary"> Wiki </span>
+      </h1>
     </div>
   );
 }
