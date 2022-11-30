246bde694f5122a3d9598b144fa7d6a04c69bad5
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-11-30 09:40:39 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
@@ -7,6 +8,15 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary"> Wiki </span>
       </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8"></div>
+        </div>
+      </div>
     </div>
   );
 }
