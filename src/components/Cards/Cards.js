15766903cddeb32871aee9b721cd06d9a3b2f8da
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-02 10:44:54 -0800

    initial commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,4 +1,7 @@
 import React from "react";
+import styles from './Cards.module.scss';
+
+
 
 const Cards = ({ results }) => {
   let display;
@@ -6,15 +9,20 @@ const Cards = ({ results }) => {
 
   if (results) {
     display = results.map((x) => {
-      let { id, name, image } = x;
+      let { id, name, image, location, status } = x;
       return (
-        <div key={id} className="col-4">
+        <div key={id} className="col-4 position-relative">
           <div className="">
             <img src={image} alt="" className="img-fluid" />
             <div className="content">
-              <div className="">{name}</div>
+              <div className="fs-5 fw-bold mb-4">{name}</div>
+              <div className="">
+                <div className="fs-6">Last Location</div>
+                <div className="fs-5">{location.name}</div>
+              </div>
             </div>
           </div>
+          <div className={`${styles.badge} position-absolute badge bg-danger`} >{status}</div>
         </div>
       );
     });
