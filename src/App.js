c5e473acccfa62b276c0335e2ef3166408546a49
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-02 08:25:18 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
 
-  let pageN
-  let api = "https://rickandmortyapi.com/api/character/?page=2";
+  console.log(fetchedData)
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
 
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
@@ -22,8 +32,7 @@ function App() {
           <div className="col-8">
             <div className="row">
               <Cards />
-              <Cards />
-              <Cards />
+              
             </div>
           </div>
         </div>
