49937bbbdfb946a2f11f17a8e1d6898ea429add5
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-02 08:52:25 -0800

    initial commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Cards = ({ results }) => {
-  console.log(results)
-  return (
-    <div className="col-4">Cards</div>
-  )
-}
+  let display;
+  console.log(results);
 
-export default Cards
+  if (results) {
+    display = results.map((x) => {
+      let { id, name, image } = x;
+      return (
+        <div key={id} className="col-4">
+          <div className="">
+            <img src={image} alt="" className="img-fluid" />
+            <div className="content">
+              <div className="">{name}</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  } else {
+    display = "No Characters Found :/";
+  }
+
+  return <>{display}</>;
+};
+
+export default Cards;
