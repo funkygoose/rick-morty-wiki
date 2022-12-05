720c86ce03f287c9ee55d105fd6269855dbcd704
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-04 23:10:54 -0800

    initial commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -11,10 +11,10 @@ const Cards = ({ results }) => {
     display = results.map((x) => {
       let { id, name, image, location, status } = x;
       return (
-        <div key={id} className="col-4 position-relative">
+        <div key={id} className="col-4 mb-4 position-relative">
           <div className={styles.cards}>
             <img src={image} alt="" className={`${styles.img} img-fluid`} />
-            <div className="content">
+            <div style={{padding: "10px"}} className="content">
               <div className="fs-5 fw-bold mb-4">{name}</div>
               <div className="">
                 <div className="fs-6">Last Location</div>
