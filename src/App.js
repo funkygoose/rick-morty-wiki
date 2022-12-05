0bde1cdf5470eff077bd40ae35001d261c955530
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-05 10:12:28 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
   let [fetchedData, updateFetchedData] = useState([]);
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   useEffect(() => {
     (async function () {
@@ -22,6 +26,8 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary"> Wiki </span>
       </h1>
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch}/>
 
       <div className="container">
         <div className="row">
@@ -36,6 +42,8 @@ function App() {
           </div>
         </div>
       </div>
+
+      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>
     </div>
   );
 }
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Pagination = ({ pageNumber ,setPageNumber }) => {
+  let next = () => {
+    setPageNumber((x) => x + 1);
+  };
+
+  let prev = () => {
+    if(pageNumber === 1) return
+    setPageNumber((x) => x - 1);
+  };
+  return (
+    <div className="container d-flex justify-content-center gap-5 my-5">
+      <button onClick={prev} className="btn btn-primary">
+        Prev
+      </button>
+      <button onClick={next} className="btn btn-primary">
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
