608fd32d344a6b5188832baf2524605968ec4b9f
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-04 23:23:12 -0800

    initial commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,5 @@
 import React from "react";
-import styles from './Cards.module.scss';
-
-
+import styles from "./Cards.module.scss";
 
 const Cards = ({ results }) => {
   let display;
@@ -14,7 +12,7 @@ const Cards = ({ results }) => {
         <div key={id} className="col-4 mb-4 position-relative">
           <div className={styles.cards}>
             <img src={image} alt="" className={`${styles.img} img-fluid`} />
-            <div style={{padding: "10px"}} className="content">
+            <div style={{ padding: "10px" }} className="content">
               <div className="fs-5 fw-bold mb-4">{name}</div>
               <div className="">
                 <div className="fs-6">Last Location</div>
@@ -22,7 +20,35 @@ const Cards = ({ results }) => {
               </div>
             </div>
           </div>
-          <div className={`${styles.badge} position-absolute badge bg-danger`} >{status}</div>
+          {(() => {
+            if (status === "dead") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute 
+                badge bg-danger`}
+                >
+                  {status}
+                </div>
+              );
+            } else if (status === "alive") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-success`}
+                >
+                  {status}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute 
+                badge bg-secondary`}
+                >
+                  {status}
+                </div>
+              );
+            }
+          })()}
         </div>
       );
     });
