72ff46324982b4667e3888057bc3d1c75d93a940
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-07 10:35:28 -0800

    initial commit

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactPaginate from "react-paginate";
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
-  return <ReactPaginate className="pagination justify-content-center" pageCount={info?.pages} />;
+  return (
+    <ReactPaginate
+      className="pagination justify-content-center gap-4 my-4"
+      forcePage={pageNumber === 1? 0 : pageNumber -1}
+      nextLabel="Next"
+      previousLabel="Prev"
+      nextClassName="btn btn-primary"
+      previousClassName="btn btn-primary"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      activeClassName="active"
+      onPageChange={(data) => {
+        setPageNumber(data.selected + 1);
+      }}
+      pageCount={info?.pages}
+    />
+  );
 };
 
 export default Pagination;
