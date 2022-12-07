3f5df5156ac26370c37bc97ece21a37369c9ae17
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-07 10:42:21 -0800

    initial commit

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Filters = () => {
   return (
     <div className="col-3">
-      <div className="text-center fw-bold fs-4 mb-4">Filter</div>
+      <div className="text-center fw-bold fs-4 mb-2">Filter</div>
+      <div style={{cursor: "pointer"}} className="text-center text-primary text-decoration-underline">Clear Filters</div>
     </div>
   )
 }
