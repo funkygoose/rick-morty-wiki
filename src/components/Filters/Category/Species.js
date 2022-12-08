5fe4fd594acfc2d05a4f3b108de5ba71398b790c
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-08 10:44:20 -0800

    initial commit

--- src/components/Filters/Category/Species.js
+++ src/components/Filters/Category/Species.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Species = () => {
   return (
     <div className="accordion-item">
-    <h2 className="accordion-header" id="headingTwo">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      <h2 className="accordion-header" id="headingTwo">
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseTwo"
+          aria-expanded="false"
+          aria-controls="collapseTwo"
+        >
+          Species
+        </button>
+      </h2>
+      <div
+        id="collapseTwo"
+        className="accordion-collapse collapse"
+        aria-labelledby="headingTwo"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body"></div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Species
+export default Species;
