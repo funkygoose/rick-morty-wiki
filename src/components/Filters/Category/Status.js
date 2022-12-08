4debf039c12f8570f7e99c6635c243cf52f48ce2
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-08 10:43:53 -0800

    initial commit

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Status = () => {
   return (
     <div className="accordion-item">
-    <h2 className="accordion-header" id="headingThree">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      <h2 className="accordion-header" id="headingThree">
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseThree"
+          aria-expanded="false"
+          aria-controls="collapseThree"
+        >
+          Status
+        </button>
+      </h2>
+      <div
+        id="collapseThree"
+        className="accordion-collapse collapse"
+        aria-labelledby="headingThree"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body"></div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Status
+export default Status;
