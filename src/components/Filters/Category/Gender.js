b2dd04f4ea0165918ffcdab3d3b1986587d4557c
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-10 18:33:58 -0800

    intial commit

--- src/components/Filters/Category/Gender.js
+++ src/components/Filters/Category/Gender.js
@@ -7,11 +7,11 @@ const Gender = ({ setPageNumber, setGender }) => {
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingOne">
         <button
-          className="accordion-button"
+          className="accordion-button collapsed"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#collapseOne"
-          aria-expanded="true"
+          aria-expanded="false"
           aria-controls="collapseOne"
         >
           Gender
@@ -19,7 +19,7 @@ const Gender = ({ setPageNumber, setGender }) => {
       </h2>
       <div
         id="collapseOne"
-        className="accordion-collapse collapse show"
+        className="accordion-collapse collapse "
         aria-labelledby="headingOne"
         data-bs-parent="#accordionExample"
       >
