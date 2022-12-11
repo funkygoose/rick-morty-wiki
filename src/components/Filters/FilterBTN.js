1b5f4ad4f0d1d208f751db0f2e58e7a2a484563e
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-10 18:05:57 -0800

    initial commit

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,21 +1,26 @@
 import React from "react";
 
-const FilterBTN = ({name, index, items }) => {
+const FilterBTN = ({ name, index, items }) => {
   return (
     <div>
+      <style jsx>
+        {`
+          input[type="radio"] {
+            display: none;
+          }
+        `}
+      </style>
       <div className="form-check">
         <input
           className="form-check-input"
           type="radio"
           name={name}
           id={`${name}-${index} `}
-
         />
         <label class="btn btn-outline-primary" for={`${name}-${index} `}>
           {items}
         </label>
       </div>
-      
     </div>
   );
 };
