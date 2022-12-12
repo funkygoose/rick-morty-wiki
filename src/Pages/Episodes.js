e460fd3ef5f955fe5f26908c607b2995573cd141
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-12 10:34:17 -0800

    initial commit

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
 
 const Episodes = () => {
   let [id, setID] = useState(1);
   let [info, setInfo] = useState([]);
+  let[results, setResults] = useState([]);
   let { air_date, name } = info;
   console.log(info);
   let api = `https://rickandmortyapi.com/api/episode/${id}`;
@@ -11,12 +13,19 @@ const Episodes = () => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
       setInfo(data);
+
+      let a = await Promise.all(
+        data.characters.map((x)=> {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
     })();
   }, [api]);
 
   return (
     <div className="container">
-      <div className="row">
+      <div className="row mb-4">
         <h1 className="text-center mb-3">
           Episode :
           <span className="text-primary">
@@ -29,8 +38,16 @@ const Episodes = () => {
         </h5>
       </div>
       <div className="row">
-        <div className="col-3">Pick Episodes</div>
-        <div className="col-8"></div>
+        <div className="col-3">
+          <div className="text-center mb-4">
+          
+          </div>
+          </div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results}/>
+          </div>
+        </div>
       </div>
     </div>
   );
