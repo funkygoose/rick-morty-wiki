41d9e57b9a1782a582be3c3986776e3ccaa27ecc
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-12 09:51:43 -0800

    initial commit

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Episodes = () => {
-  return (
-    <div>
-      The episodes are here
-    </div>
-  )
-}
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let { air_date, name } = info;
+  console.log(info);
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
 
-export default Episodes
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+    })();
+  }, [api]);
+
+  return ( 
+  <div className="container">
+      <div className="row">
+        <h1 className="text-center"></h1>
+        <h5 className="text-center">
+          Air Date {air_date}
+        </h5>
+      </div>
+      <div className="row"></div>
+  </div>
+  );
+};
+
+export default Episodes;
