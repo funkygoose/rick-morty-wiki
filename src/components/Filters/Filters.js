bfdbd096b9b9d001b77e455d3e4e621e0f94cb78
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-11 22:17:35 -0800

    initial commit

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Gender from "../Filters/Category/Gender"
-import Species from "../Filters/Category/Species"
-import Status from "../Filters/Category/Status"
+import Gender from "../Filters/Category/Gender";
+import Species from "../Filters/Category/Species";
+import Status from "../Filters/Category/Status";
 
-const Filters = ({setStatus, setPageNumber, setGender, setSpecies}) => {
+const Filters = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
+  let clear = () => {
+    setStatus("");
+    setPageNumber("");
+    setGender("");
+    setSpecies("");
+    window.location.reload(false);
+  };
   return (
     <div className="col-3">
       <div className="text-center fw-bold fs-4 mb-2">Filter</div>
       <div
+        onClick={clear}
         style={{ cursor: "pointer" }}
         className="text-center text-primary text-decoration-underline mb-4"
       >
@@ -15,9 +23,9 @@ const Filters = ({setStatus, setPageNumber, setGender, setSpecies}) => {
       </div>
 
       <div className="accordion" id="accordionExample">
-        <Status setPageNumber={setPageNumber} setStatus={setStatus}/>
-        <Species setPageNumber={setPageNumber} setSpecies={setSpecies}/>
-        <Gender setGender={setGender} setPageNumber={setPageNumber}/>
+        <Status setPageNumber={setPageNumber} setStatus={setStatus} />
+        <Species setPageNumber={setPageNumber} setSpecies={setSpecies} />
+        <Gender setGender={setGender} setPageNumber={setPageNumber} />
       </div>
     </div>
   );
