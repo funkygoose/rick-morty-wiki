cb9682665b91c4f6f1c9d91476bf76d22eae9f35
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-11 23:10:37 -0800

    initial commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
-
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container">
-      <Link to="/" className="fs-3 ubuntu navbar-brand">
-        Rick & Morty <span className="text-primary"> Wiki </span>
-      </Link>
+        <Link to="/" className="fs-3 ubuntu navbar-brand">
+          Rick & Morty <span className="text-primary"> Wiki </span>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -18,14 +17,18 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <i class="fas fa-bars open"></i>
+          <i class="fas fa-times close"></i>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
           <div className="navbar-nav fs-5">
-            <NavLink to ="/" className="nav-link" >
+            <NavLink to="/" className="nav-link">
               Characters
             </NavLink>
-            <NavLink to="/episodes"className="nav-link" >
+            <NavLink to="/episodes" className="nav-link">
               Episodes
             </NavLink>
             <NavLink to="/Location" className="nav-link">
