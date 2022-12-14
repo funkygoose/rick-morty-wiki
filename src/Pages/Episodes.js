345fa33f0148ffd465b413d950e6d04ce1a51a3a
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-12-14 09:54:34 -0800

    initial commit

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -40,7 +40,7 @@ const Episodes = () => {
       <div className="row">
         <div className="col-3">
           <h4 className="text-center mb-4">Pick Episodes</h4>
-          <InputGroup name="Episode" total={51} />
+          <InputGroup setID={setID} name="Episode" total={51} />
         </div>
         <div className="col-8">
           <div className="row">
--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-const InputGroup = ({ total, name }) => {
+const InputGroup = ({ total, name, setID }) => {
   return (
     <div class="input-group mb-3">
-      <select class="form-select" id="inputGroupSelect01">
+      <select 
+      onChange={e=>setID(e.target.value)}
+      class="form-select" id={name}>
         <option selected>Choose...</option>
 
         {[...Array(total).keys()].map((x) => {
-          return <option value={x + 1}>{x + 1}</option>;
+          return <option value={x + 1}>{name} - {x + 1}</option>;
         })}
       </select>
     </div>
